db1510b1a0686e40d62a064315da5e09aeb8d87c
Luciano <luciano.vittoretti@gmail.com>
2023-04-15 16:25:44 -0300

    Removing methods to test generation of token with fake user.

--- application/controllers/api/Test_api.php
+++ application/controllers/api/Test_api.php
@@ -1,44 +1,21 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require(APPPATH.'/libraries/REST_Controller.php');
+require(APPPATH . '/libraries/REST_Controller.php');
+
 use Restserver\Libraries\REST_Controller;
+
 class Test_api extends REST_Controller
 {
 	public function __construct()
 	{
-		parent::__construct();	
-		$this->load->library('Authorization_Token');	  
-	}
-	public function tokenGen_post()
-	{   
-		$token_data['user_id'] = 1001;
-		$token_data['fullname'] = 'Hello World'; 
-		$token_data['email'] = 'helloworld@gmail.com';
-
-		$tokenData = $this->authorization_token->generateToken($token_data);
-
-		$final = array();
-		$final['token'] = $tokenData;
-		$final['status'] = 'ok';
-
-		$this->response($final); 
-	}
-	public function verify_post()
-	{  
-		$headers = $this->input->request_headers(); 
-		if (isset($headers['jwt-authorization'])) {
-			$decodedToken = $this->authorization_token->validateToken($headers['jwt-authorization']);
-			$this->response($decodedToken);
-		}
-		else {
-			$this->response(['Authentication failed'], REST_Controller::HTTP_OK);
-		}
-			
-		  
+		parent::__construct();
+		$this->load->library('Authorization_Token');
 	}
 
-
- 
+	public function verify_token_post()
+	{
+		$decodedToken = $this->authorization_token->validateToken();
+		$this->response($decodedToken);
+	}
 }
-
